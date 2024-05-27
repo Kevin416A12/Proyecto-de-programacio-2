e303e57a19ba18c2281ed4b85b6e81ed73a890fe
Kevin416A12 <kevin.c.41683@gmail.com>
2024-05-26 20:57:59 -0600

    Creacion de la clase y un testeo para mostrar la matriz

--- Codigo.py
+++ Codigo.py
@@ -1,58 +1,59 @@
 import pygame
 
-
 pygame.init()
 
-screen_width = 800
-screen_heigt = 600
+screen_width = 400
+screen_height = 400
 
-screen = pygame.display.set_mode((screen_width , screen_heigt))
-run = True
-while run :
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-pygame.quit()
+screen = pygame.display.set_mode((screen_width, screen_height))
 
+class Editor:
 
-
-
-
-class editor:
     def __init__(self):
+        self.colores = {
+            0: (255, 255, 255),  # Blanco
+            1: (0, 0, 0),        # Negro
+            2: (255, 0, 0),      # Rojo
+            3: (0, 255, 0),      # Verde
+            4: (0, 0, 255),      # Azul
+    }
+
         self.matriz = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1],
+            [1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2],
+            [2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3],
+            [3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4],
+            [4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0],
+            [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1],
+            [1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2],
+            [2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3],
+            [3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4],
+            [4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0],
+            [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1],
+            [1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2],
     ]
+        self.tamano_celda = 30
 
 
+    def dibujar_matriz(self):
+        for y, row in enumerate(self.matriz):
+            for x, value in enumerate(row):
+                color = self.colores.get(value, (255, 255, 255))  # Color por defecto: blanco
+                rect = pygame.Rect(x * self.tamano_celda, y * self.tamano_celda, self.tamano_celda, self.tamano_celda)
+                pygame.draw.rect(screen, color, rect)
 
+editor = Editor()
 
-
-
-
-
-
-
-pygame.init()
-
-screen_width = 800
-screen_heigt = 600
-
-screen = pygame.display.set_mode((screen_width , screen_heigt))
 run = True
-while run :
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+
+    screen.fill((255, 255, 255 ))
+
+    editor.dibujar_matriz()
+
+    pygame.display.flip()
 pygame.quit()
